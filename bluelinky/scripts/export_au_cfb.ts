f4eb83c12f8e0de4222b30f125c067c695af796f
adrianchui <66452200+adrianchui@users.noreply.github.com>
2025-11-13 00:02:35 -0500

    Delete bluelinky directory

--- bluelinky/scripts/export_au_cfb.ts
+++
@@ -1,6 +0,0 @@
-import { getBrandEnvironment } from '../src/constants/australia';
-import { exportCfb } from './export_cfb';
-
-exportCfb('hacksore/hks:native-au', 'australia.cfb.ts', getBrandEnvironment).catch(e =>
-  console.error(e)
-);
--- bluelinky/scripts/export_eu_cfb.ts
+++
@@ -1,4 +0,0 @@
-import { getBrandEnvironment } from '../src/constants/europe';
-import { exportCfb } from './export_cfb';
-
-exportCfb('hacksore/hks:native', 'europe.cfb.ts', getBrandEnvironment).catch(e => console.error(e));
